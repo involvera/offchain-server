70fcdd6f11e2cf09d7e5345da4196863421981fe
fantasim <siuol@gmx.com>
2021-09-23 09:33:38 +0300

    sort

--- src/routes/proposal/methods.ts
+++ src/routes/proposal/methods.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { proposal } from '../../models'
+import { proposal, ProposalCollection } from '../../models'
 
 export const GetProposalList = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const { page } = req.headers
 
     try {
         const list = await proposal.pullBySID(parseInt(req.params.sid), !page ? 0 : parseInt(page as string))
-        res.status(200).json(await list.renderJSON('preview'))
+        const l = list.local().orderBy('index', 'desc') as ProposalCollection
+        res.status(200).json(await l.renderJSON('preview'))
     } catch (e){
         res.status(500).json(e.toString())
     }
